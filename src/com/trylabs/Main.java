34a6360f89ef2d25fc6930c8242eaa760c0058c7
Sandro Whitehorse <sg3802307@gmail.com>
2017-06-12 20:55:14 +0300

    Initial commit

---
+++ src/com/trylabs/Main.java
@@ -0,0 +1,13 @@
+package com.trylabs;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int i;
+        int sum=0;
+        sum=sum+1;
+	for (i=1;i<10;i++)
+        {sum=sum+4;}
+        System.out.println(sum);
+    }
+}
